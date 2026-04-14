97d9bc9ac1205870724b463c1c12401fbe0f08a6
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-04-14 13:11:22 +0530

    Added test class and update pom.xml

--- src/main/java/com/ci/DemoApp/Calculator.java
+++ src/main/java/com/ci/DemoApp/Calculator.java
@@ -1,4 +1,14 @@
 package com.ci.DemoApp;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
---
+++ src/test/java/com/ci/DemoApp/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.ci.DemoApp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(2, calc.subtract(5, 3));
+    }
+}
